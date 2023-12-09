511532a33b3c3b8009abbe7178a13c5ed113adaf
Premysl Fara <premysl.fara@gmail.com>
2023-12-09 18:21:23 +0100

    Initial commit.

---
+++ HelloOpenTk/Game.cs
@@ -0,0 +1,31 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+namespace HelloOpenTk;
+
+public class Game : GameWindow
+{
+    public Game(int width, int height, string title)
+        : base(GameWindowSettings.Default, new NativeWindowSettings()
+        {
+            ClientSize = (width, height),
+            Title = title
+        })
+    {
+        
+    }
+    
+    
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+        
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+}
---
+++ HelloOpenTk/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloOpenTk;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        // This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
+        using (var game = new Game(800, 600, "LearnOpenTK"))
+        {
+            game.Run();
+        }
+    }
+}
