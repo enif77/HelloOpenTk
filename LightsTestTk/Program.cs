1c473171453e5e7cfd1c3745d1db84b6a2c723e8
Premysl Fara <premysl.fara@gmail.com>
2023-12-28 10:42:17 +0100

    Multiple lights test app.

--- LightsTestTk/Program.cs
+++ LightsTestTk/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
 
-Console.WriteLine("Hello, World!");
+namespace LightsTestTk
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Multiple lights",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
