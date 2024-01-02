f1d794d892e782fe77c8304df2cc14d021723595
Premysl Fara <premysl.fara@gmail.com>
2024-01-02 09:15:56 +0100

    Settings are loaded from JSON.

--- LightsTestTk/Program.cs
+++ LightsTestTk/Program.cs
@@ -1,4 +1,8 @@
-﻿using OpenTK.Mathematics;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+
+using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
@@ -17,6 +21,8 @@ public static class Program
     
     private static void Main()
     {
+        Configure();
+        
         var nativeWindowSettings = new NativeWindowSettings()
         {
             ClientSize = new Vector2i(Settings.WindowWidth, Settings.WindowHeight),
@@ -35,5 +41,44 @@ public static class Program
         {
             window.Run();
         }
+    }
+
+    
+    private static void Configure()
+    {
+        const string configFileName = Defaults.ConfigFileName;
+        
+        // We are reading and writing config file in user's home directory.
+        var configFileRootPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        
+        // If config file does not exist, create it with default values.
+        var configPath = Path.Combine(
+            configFileRootPath,
+            configFileName);
+        if (File.Exists(configPath) == false)
+        {
+            try
+            {
+                File.WriteAllText(configPath, Settings.DefaultSettings.ToJson());
+            }
+            catch (IOException)
+            {
+                // We are OK with an IO related exception.
+            }
+        }
+        
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile(
+                new PhysicalFileProvider(      // This provider allows us to load config from an absolute path.
+                    configFileRootPath,
+                    ExclusionFilters.System),  // We are reading from an hidden file.
+                configFileName,
+                optional: true,
+                reloadOnChange: false);  // This slows down app startup. Also - not sure, how to react to config reloads.
+
+        var config = builder.Build();
+
+        Settings = config.GetSection("Settings")
+            .Get<Settings>() ?? Settings.DefaultSettings;
     }
 }
